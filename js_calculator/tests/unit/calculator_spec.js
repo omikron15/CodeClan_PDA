f9ec13870a2dad0dcf39089895762589587adfe1
omikron15 <omikron15@hotmail.co.uk>
2018-06-23 16:19:36 +0100

    Basic unit testing completed

--- js_calculator/tests/unit/calculator_spec.js
+++ js_calculator/tests/unit/calculator_spec.js
@@ -6,9 +6,53 @@ describe('calculator', function () {
     calculator = new Calculator()
   });
 
-  // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
-    assert.equal(true, true)
+  it('it can add 1 and 5', function(){
+    calculator.numberClick(1);
+    calculator.operatorClick('+')
+    calculator.numberClick(5);
+    calculator.operatorClick('=')
+    assert.equal(calculator.runningTotal, 6)
+  })
+
+  it('it can subtract 4 from 7', function(){
+    calculator.numberClick(7);
+    calculator.operatorClick('-')
+    calculator.numberClick(4);
+    calculator.operatorClick('=')
+    assert.equal(calculator.runningTotal, 3)
+  })
+
+  it('it can multiply 3 and 5', function(){
+    calculator.numberClick(3);
+    calculator.operatorClick('*')
+    calculator.numberClick(5);
+    calculator.operatorClick('=')
+    assert.equal(calculator.runningTotal, 15)
+  })
+
+  it('it can divide 21 by 7', function(){
+    calculator.numberClick(21);
+    calculator.operatorClick('/')
+    calculator.numberClick(7);
+    calculator.operatorClick('=')
+    assert.equal(calculator.runningTotal, 3)
+  })
+
+  it('it can select a number', function(){
+    calculator.numberClick(5);
+    assert.equal(calculator.runningTotal, 5)
+  })
+
+  //unsure how to test operator selection ?
+  //this is tested in all other tests anyways?
+
+  // it('it can select an operator', function(){
+  //   assert.equal(false, true)
+  // })
+
+  it('it can clear the selected number', function(){
+    calculator.clearClick();
+    assert.equal(calculator.runningTotal, 0)
   })
 
 });
