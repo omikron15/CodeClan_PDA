6e83f77b7c911794b92d92a6516d3b0a5adca564
omikron15 <omikron15@hotmail.co.uk>
2018-06-23 16:47:59 +0100

    Adds integration test for dividing by zero error

--- js_calculator/tests/integration/tests.js
+++ js_calculator/tests/integration/tests.js
@@ -88,4 +88,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('100')
   })
 
+  it('should not be able to divide by zero', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('err')
+  })
+
 });
